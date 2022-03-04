d5fa22f08bb2b552c1b10cb1ba755358fa277c9a
Borna <bnovak@wustl.edu>
2022-03-04 11:09:42 -0600

    Added the right seed to recreate the variability in scores

--- docking.py
+++ docking.py
@@ -3,12 +3,12 @@ from vina import Vina
 receptor = './state000529.pdbqt'
 ligand = './ligand-9.pdbqt'
 exhaustiveness=32
-    
-v = Vina(sf_name='vina')
+
+v = Vina(sf_name='vina',seed=405008068)
 v.set_receptor(receptor)
 v.set_ligand_from_file(ligand)
 v.compute_vina_maps(center=[52.45, 44.2, 39.51], box_size=[15, 15, 15])
 v.dock(exhaustiveness=exhaustiveness)
-v.write_poses('ligand-91.pdbqt', n_poses=1)
+v.write_poses('ligand-91.pdbqt', n_poses=1,overwrite=True)
 v.dock(exhaustiveness=exhaustiveness)
-v.write_poses('ligand-92.pdbqt', n_poses=1)
+v.write_poses('ligand-92.pdbqt', n_poses=1,overwrite=True)
